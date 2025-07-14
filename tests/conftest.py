9e394e87fc9cf7daee068f78a98713a34949a416
aakuzmh <tipokot@gmail.com>
2025-07-14 12:03:05 +0300

    Убрал ассерт из фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ def user_with_order(registered_user):
         "ingredients": ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"]
     }
     response = requests.post(f"{BASE_URL}/orders", json=order_payload, headers=registered_user["headers"])
-    assert response.status_code == 200, f"Failed to create order: {response.status_code} {response.text}"
     
     return registered_user["headers"]
 
