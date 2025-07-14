a410c57a5f859f195fde0c2b7fdafed8f7e1d44c
aakuzmh <tipokot@gmail.com>
2025-07-14 12:07:44 +0300

    Убрал ингридиенты в data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,12 @@
 import pytest
 import requests
-from data import BASE_URL
+from data import BASE_URL, VALID_INGREDIENTS
 from helpers import create_test_user, delete_test_user
 
 @pytest.fixture
 def user_with_order(registered_user):
     # Create an order for the registered user
-    order_payload = {
-        "ingredients": ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"]
-    }
-    response = requests.post(f"{BASE_URL}/orders", json=order_payload, headers=registered_user["headers"])
+    requests.post(f"{BASE_URL}/orders", json={"ingredients": VALID_INGREDIENTS}, headers=registered_user["headers"])
     
     return registered_user["headers"]
 
--- tests/data.py
+++ tests/data.py
@@ -1,1 +1,6 @@
 BASE_URL = "https://stellarburgers.nomoreparties.site/api"
+
+VALID_INGREDIENTS = [
+    "61c0c5a71d1f82001bdaaa6d",
+    "61c0c5a71d1f82001bdaaa6f"
+]
