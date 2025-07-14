580e51648b423b58e5cb02cd76a345da8e537975
aakuzmh <tipokot@gmail.com>
2025-07-14 12:00:53 +0300

    Переписал фикстуру регистрации, убрал логику в helpers.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,7 @@
 import pytest
 import requests
-import random
 from data import BASE_URL
-
-@pytest.fixture
-def registered_user():
-    email = f"user{random.randint(1000, 9999)}@example_07.com"
-    password = "password123"
-    name = "Test User"
-    payload = {
-        "email": email,
-        "password": password,
-        "name": name
-    }
-
-    response = requests.post(f"{BASE_URL}/auth/register", json=payload)
-    assert response.status_code == 200
-    token = response.json().get("accessToken")
-    assert token is not None
-
-    user_data = {
-        "email": email,
-        "password": password,
-        "accessToken": token,
-        "headers": {"Authorization": token}
-    }
-
-    yield user_data
-
-    delete_response = requests.delete(f"{BASE_URL}/auth/user", headers=user_data["headers"])
-    assert delete_response.status_code == 202, f"Failed to delete user: {delete_response.status_code} {delete_response.text}"
+from helpers import create_test_user, delete_test_user
 
 @pytest.fixture
 def user_with_order(registered_user):
@@ -41,3 +13,9 @@ def user_with_order(registered_user):
     assert response.status_code == 200, f"Failed to create order: {response.status_code} {response.text}"
     
     return registered_user["headers"]
+
+@pytest.fixture
+def registered_user():
+    user = create_test_user()
+    yield user
+    delete_test_user(user["headers"])
